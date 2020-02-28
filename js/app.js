5d77d0aada5c1dceec4eb4b3a644ce3a0b040510
hhhoang <thimyhanh.hoang@gmail.com>
2020-02-28 17:53:50 +0100

    update movement of player based on arrow keys

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 //canvas.width = 505;
 //canvas.height = 606;
 
+// ******** ENEMIES ********
 // Enemies our player must avoid
 var Enemy = function(x,y,s) {
     // Variables applied to each of our instances go here,
@@ -45,8 +46,47 @@ Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 // a handleInput() method.
-Player.prototype.handleInput = function(){
+// move the player subject to the movement of arrow key 
+Player.prototype.handleInput = function(key){
+    console.log(key);
+    switch(key){
+        case "left":
+            if(this.x - 50 >= 0){
+                this.x -= 50;
+            } else{
+                this.x = 0;
+            }
+            break;
+        case "right":
+            if(this.x + 50 <= 450){
+                this.x += 50;
+            } else{
+                this.x = 450;
+            }    
 
+            break;
+        case "up":
+            if(this.y - 50 >= 0){
+                this.y -= 50;
+            } else{
+                this.y = 0;
+                // player reaches water
+            }             
+            break;
+        case "down":
+            if(this.y + 50 <= 450){
+                this.y += 50;
+                console.log(this.x, this.y);
+            } else{
+                this.y = 450;
+            }
+            break;    
+    }
+    // player reaches water
+    if(this.y == 0){
+        console.log("reach water")
+        resetPlayerPosition();
+    }
 }
 
 
@@ -65,6 +105,13 @@ allEnemies.push(enemy3);
 // Place the player object in a variable called player
 var player = new Player(100,400,20);
 
+
+// TO-DO:
+// When player reaches the water without colliding, return to initial position
+function resetPlayerPosition(){
+    player.x = 100;
+    player.y = 400;
+}
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
