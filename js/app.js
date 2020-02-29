82ead70357c0c21880b5429e628fff1fb332f43a
hhhoang <thimyhanh.hoang@gmail.com>
2020-02-29 16:18:56 +0100

    update position of bugs

--- js/app.js
+++ js/app.js
@@ -21,7 +21,10 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x += this.speed * dt
+    this.x += this.speed * dt;
+    if (this.x > 450) {
+        this.x = -50;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
